0895064ba61af7ff0bc2f6a471484100a8fac552
root <root@ip-172-31-93-53.ec2.internal>
2019-01-14 03:46:09 +0000

    node js application

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world Rich and Eugene gonna go hard\n');
+  res.send('Hello world Rich Go Hard or Go home!!!\n');
 });
 
 app.listen(PORT, HOST);
