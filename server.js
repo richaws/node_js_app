73aab5c880d630d9734c4c5f9191073963953184
EC2 Default User <ec2-user@ip-172-31-91-135.ec2.internal>
2019-01-15 02:01:06 +0000

    testing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world Rich Go Hard or Go home!!!\n');
+  res.send('Hello world Eugene Go Hard or Go home!!!\n');
 });
 
 app.listen(PORT, HOST);
